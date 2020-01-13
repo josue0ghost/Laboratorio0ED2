3c4407f096fa574d267fca24efb5960eb05446ea
Emmanuel Alvarado <josue0ghost@gmail.com>
2020-01-13 15:59:40 -0600

    Update MovieController.cs
    Added a static readonly Movie array

--- API/API/Controllers/MovieController.cs
+++ API/API/Controllers/MovieController.cs
@@ -12,11 +12,25 @@ namespace API.Controllers
     [ApiController]
     public class MovieController : ControllerBase
     {
+        private static readonly Movie[] movies = new[]
+        {
+            new Movie {Id = 1, Director = "Me", Name = "Memento", Year = 2013 },
+            new Movie{Id = 2, Director = "Cristopher Nolan", Name = "Batman", Year = 2010},
+            new Movie{Id = 3, Director = "D1", Name = "N1", Year = 2000},
+            new Movie{Id = 4, Director = "D2", Name = "N2", Year = 2001},
+            new Movie{Id = 5, Director = "D3", Name = "N3", Year = 2002},
+            new Movie{Id = 6, Director = "D4", Name = "N4", Year = 2003},
+            new Movie{Id = 7, Director = "D5", Name = "N5", Year = 2004},
+            new Movie{Id = 8, Director = "D6", Name = "N6", Year = 2005},
+            new Movie{Id = 9, Director = "D7", Name = "N7", Year = 2006},
+            new Movie{Id = 10, Director = "DA1", Name = "NA1", Year = 2007}
+        };
+
         // GET: api/Movie
         [HttpGet]
-        public IEnumerable<Movie> Get()
+        public IEnumerable<string> Get()
         {
-            return new Movie[] { new Movie { Director = "Me", Name = "Memento", Year = 2013 } };
+            return new[] { "value", "value1" };
         }
 
         // GET: api/Movie/5
